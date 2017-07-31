e6e7d6b52ab228f14ba6856714e28ca18a64aee9
p-agapov <p.a.agapov@gmail.com>
2017-07-31 17:21:37 +0300

    Level 11 complete

--- 1.JavaSyntax/src/com/javarush/task/task10/task1020/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1020/Solution.java
@@ -22,7 +22,7 @@ public class Solution {
     }
 
     public static void sort(int[] array) {
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] > array[j]) {
                     int buffer = array[i];
---
+++ 2.JavaCore/src/com/javarush/task/task11/task1101/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task11.task1101;
+
+/*
+Лошадь и Пегас
+*/
+
+public class Solution {
+    public static void main(final String[] args) {
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegas extends Horse {
+
+    }
+}
