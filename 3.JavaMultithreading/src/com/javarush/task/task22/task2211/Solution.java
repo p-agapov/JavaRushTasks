48ae2d51324415941b9f61d36eb8bdc44c458da6
p-agapov <p.a.agapov@gmail.com>
2018-04-19 20:08:58 +0300

    Task 2211 of level 22 complete

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2211/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task22.task2211;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+/*
+Смена кодировки
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (
+                FileInputStream inputStream = new FileInputStream(args[0]);
+                FileOutputStream outputStream = new FileOutputStream(args[1])
+        ) {
+            Charset windows1251 = Charset.forName("Windows-1251");
+            Charset utf8 = Charset.forName("UTF-8");
+
+            byte[] buffer = new byte[inputStream.available()];
+            inputStream.read(buffer);
+            String s = new String(buffer, windows1251);
+            buffer = s.getBytes(utf8);
+            outputStream.write(buffer);
+        }
+    }
+}
