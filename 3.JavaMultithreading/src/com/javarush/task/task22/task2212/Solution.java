cd86a677137487347a329fa2aad608be910974a3
p-agapov <p.a.agapov@gmail.com>
2018-04-20 22:14:37 +0300

    Task 2212 of level 22 complete

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task22.task2212;
+
+/*
+Проверка номера телефона
+*/
+public class Solution {
+    public static boolean checkTelNumber(String telNumber) {
+        if (telNumber == null) return false;
+        return telNumber.matches("((?:\\+\\d{2})?\\(?\\d{3}\\)?\\d{3}-?\\d{2}-?\\d{2})");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkTelNumber(null));
+        System.out.println(checkTelNumber("+380501234567"));
+        System.out.println(checkTelNumber("+38(050)1234567"));
+        System.out.println(checkTelNumber("+38050123-45-67"));
+        System.out.println(checkTelNumber("050123-4567"));
+        System.out.println(checkTelNumber("+38)050(1234567"));
+        System.out.println(checkTelNumber("+38(050)1-23-45-6-7"));
+        System.out.println(checkTelNumber("050ххх4567"));
+        System.out.println(checkTelNumber("050123456"));
+        System.out.println(checkTelNumber("(0)501234567"));
+    }
+}
